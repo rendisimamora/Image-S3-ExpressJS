f58c22135ea01b045dd2b376456f6884a59a4c5d
Rendi Simamora <rendisimamora7127@gmail.com>
2018-07-30 22:45:24 +0700

    Add function upload image to S3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const busboyBodyParser = require('busboy-body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(busboyBodyParser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const AWS = require('aws-sdk')
+
+const BUCKET_NAME = 'lunadorii-dev'
+const IAM_USER_KEY = 'AKIAIDZ3JEHIHGIIFKDA'
+const IAM_USER_SECRET = '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'
+const REGION = 'ap-southeast-1'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload-image', function(req, res, next){
+    let s3bucket = new AWS.S3({
+      accessKeyId: IAM_USER_KEY,
+      secretAccessKey: IAM_USER_SECRET,
+      Bucket: BUCKET_NAME
+    });
+    s3bucket.createBucket(function () {
+      var params = {
+        Bucket: BUCKET_NAME,
+        Region: REGION,
+        Key: 'avatars/' + Date.now() + ".jpg",
+        Body: req.files.avatar.data,        
+        ACL: 'public-read'
+      };
+      s3bucket.upload(params, function (err, data) {
+       if (err) {
+        console.log('error in callback');
+        console.log(err);
+       }else{
+         console.log(data)
+       }
+      });
+    })
+})
 
 module.exports = router;
